95161f7a5cc1c22d6527759ef4284896adcbb4ba
DaniloPinheiro91 <dpinheiro.profissional@hotmail.com>
2026-04-11 09:15:02 -0300

    texture add

--- parse/parse_texture.c
+++ parse/parse_texture.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parse_texture.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: renato <renato@student.42.fr>              +#+  +:+       +#+        */
+/*   By: dapinhei <dapinhei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/07 10:17:50 by dapinhei          #+#    #+#             */
-/*   Updated: 2026/04/10 22:42:31 by renato           ###   ########.fr       */
+/*   Updated: 2026/04/11 07:25:39 by dapinhei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,13 +58,13 @@ void	parse_texture(char *line, t_map *map, t_mlx *mlx)
 		exit(1);
 	}
 	clean_newline(s[1]);
-	if (!ft_strncmp(s[0], "NO", 3))
+	if (!ft_strncmp(s[0], "NO", 2))
 		load_texture(mlx, &map->north, s[1]);
-	else if (!ft_strncmp(s[0], "SO", 3))
+	else if (!ft_strncmp(s[0], "SO", 2))
 		load_texture(mlx, &map->south, s[1]);
-	else if (!ft_strncmp(s[0], "WE", 3))
+	else if (!ft_strncmp(s[0], "WE", 2))
 		load_texture(mlx, &map->west, s[1]);
-	else if (!ft_strncmp(s[0], "EA", 3))
+	else if (!ft_strncmp(s[0], "EA", 2))
 		load_texture(mlx, &map->east, s[1]);
 	free_split(s);
 }
